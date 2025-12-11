2c754d4b9307616b72cc443864112742672fc01d
JoshuaBOUCHAT <joshuabouchat@gmail.com>
2025-12-11 21:08:41 +0100

    working

--- src/transactions/transaction.rs
+++ src/transactions/transaction.rs
@@ -1,17 +1,16 @@
-
 use ed25519_dalek::{Signature, SigningKey, VerifyingKey, ed25519::signature::SignerMut};
 use sha2::{Digest, Sha256};
 
 use crate::{
     shared::Hash,
     transactions::{transaction_input::Input, transaction_output::Output},
-    utxo_map::{ UTXOMap},
+    utxo_map::UTXOMap,
 };
 
 pub struct RawTransaction {
     inputs: Vec<Input>,
     outputs: Vec<Output>,
-    pubkey:VerifyingKey
+    pubkey: VerifyingKey,
 }
 impl RawTransaction {
     pub fn inputs(&self) -> &[Input] {
@@ -33,6 +32,9 @@ impl RawTransaction {
     }
     pub fn sign(self, sign_key: &mut SigningKey) -> SignedTransaction {
         SignedTransaction::from_raw(self, sign_key)
+    }
+    pub fn get_pubkey(&self) -> &VerifyingKey {
+        &self.pubkey
     }
 }
 
@@ -64,27 +66,70 @@ impl SignedTransaction {
             signature,
         }
     }
+    fn get_pubkey(&self) -> &VerifyingKey {
+        self.raw.get_pubkey()
+    }
 }
 
-pub struct ValidatedTransaction {}
+#[repr(transparent)]
+pub struct ValidatedTransaction {
+    transaction: SignedTransaction,
+}
 
 pub enum TransactionValidationError {
     InsufficientInput,
     InsufficientOutput,
     InputInvalid,
-    UnauthorizedInput
+    UnauthorizedInput,
 }
 
 impl ValidatedTransaction {
     pub fn validate(
         signed_transaction: SignedTransaction,
-        utxo_map: UTXOMap,
+        utxo_map: &UTXOMap,
     ) -> Result<Self, TransactionValidationError> {
-        for input in signed_transaction.inputs() {
+        let total_input = Self::sum_and_validat_inputs(
+            signed_transaction.inputs(),
+            signed_transaction.get_pubkey(),
+            utxo_map,
+        )?;
+        let total_output: u64 = signed_transaction
+            .outputs()
+            .iter()
+            .map(|output| output.get_amount())
+            .sum();
+        Self::check_balance(total_input, total_output)?;
+
+        Ok(Self {
+            transaction: signed_transaction,
+        })
+    }
+    fn sum_and_validat_inputs(
+        inputs: &[Input],
+        pubkey: &VerifyingKey,
+        utxo_map: &UTXOMap,
+    ) -> Result<u64, TransactionValidationError> {
+        let mut input_sum = 0;
+        for input in inputs {
             let Some(output) = utxo_map.try_find_matching_output(input) else {
                 return Err(TransactionValidationError::InputInvalid);
             };
-            if output.get_pubkey()!=signed_transaction.signature.
+            if output.get_pubkey() != pubkey {
+                return Err(TransactionValidationError::UnauthorizedInput);
+            }
+            input_sum += output.get_amount()
+        }
+        Ok(input_sum)
+    }
+    fn check_balance(
+        total_input: u64,
+        total_output: u64,
+    ) -> Result<(), TransactionValidationError> {
+        use std::cmp::Ordering;
+        match total_input.cmp(&total_output) {
+            Ordering::Equal => Ok(()),
+            Ordering::Less => Err(TransactionValidationError::InsufficientInput),
+            Ordering::Greater => Err(TransactionValidationError::InsufficientOutput),
         }
     }
 }
